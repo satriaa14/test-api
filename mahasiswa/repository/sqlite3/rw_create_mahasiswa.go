e6146f6985031e5fb96377911d4715d1ec5c1bc0
Agung Satria <satria_agung14@yahoo.com>
2021-04-08 11:56:29 +0700

    Add

--- mahasiswa/repository/sqlite3/rw_create_mahasiswa.go
+++ mahasiswa/repository/sqlite3/rw_create_mahasiswa.go
@@ -1,6 +1,9 @@
 package sqlite3
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/satriaa14/test-api/mahasiswa/model"
 )
 
@@ -8,6 +11,9 @@ func (rw *sqLiteReadWriter) CreateMahasiswa(req model.Mahasiswa) error {
 
 	_, err := rw.sqLite.Exec(insertMahasiswa, req.NIM, req.Name, req.Class, req.Phone, req.CreatedAt, req.CreatedBy, req.UpdatedAt, req.UpdatedBy)
 	if err != nil {
+		if strings.Contains(err.Error(), "UNIQUE constraint failed:") {
+			return fmt.Errorf("NIM %s is already exist, try another NIM", req.NIM)
+		}
 		return err
 	}
 
